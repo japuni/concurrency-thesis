cbab44565935c48ca2c0fa0ad0c66fd752af5b7f
Adam <adsal92@hotmail.com>
2024-04-05 15:29:21 +0200

    Matrix typ klar?

--- Erlang/matrix.erl
+++ Erlang/matrix.erl
@@ -4,8 +4,8 @@
 test(N) ->
     A = generate_matrix(N),
     B = generate_matrix(N),
-    FlippedMatrix = flip_matrix(B),
     Start = erlang:monotonic_time(nanosecond),
+    FlippedMatrix = flip_matrix(B),
     multiply(A, FlippedMatrix),
     End = erlang:monotonic_time(nanosecond),
     Duration = (End - Start) / 1_000_000_000,
@@ -14,8 +14,8 @@ test(N) ->
 test_parallel(N, AmountOfWorkers) ->
     A = generate_matrix(N),
     B = generate_matrix(N),
-    FlippedMatrix = flip_matrix(B),
     Start = erlang:monotonic_time(nanosecond),
+    FlippedMatrix = flip_matrix(B),
     multiply_parallel(A, FlippedMatrix, AmountOfWorkers,N),
     End = erlang:monotonic_time(nanosecond),
     Duration = (End - Start) / 1_000_000_000,
@@ -26,16 +26,36 @@ multiply(MatrixA, MatrixB) ->
     multiply_matrices(MatrixA, MatrixB,[]).
 
 multiply_parallel(MatrixA, MatrixB, Workers, N) ->
-    Master = self(),
     SplitValue = N div Workers,
-    WorkPool = spawn(fun() -> 
-        worker_pool(Master, MatrixA, [], MatrixB, SplitValue) end),
-    [spawn(fun() -> worker(WorkPool) end) || _ <- lists:seq(1, Workers)],
+    WorkerPids = [spawn(fun() -> worker(MatrixB) end) || _ <- lists:seq(1, Workers)],
+    WorkList = split_matrix(MatrixA, SplitValue, []),
+    io:format("Antal Workers = ~p~n Antal Work = ~p~n", [length(WorkerPids), length(WorkList)]),
+    send_work(WorkList, WorkerPids),
+    gather_result(WorkerPids, []).
+
+send_work([], []) ->
+    ok;
+send_work([Work | RestOfWork], [WorkerPid | RestOfWorkers]) ->
+    WorkerPid ! {work, self(), Work},
+    send_work(RestOfWork, RestOfWorkers).
+
+worker(Matrix) ->
+    receive 
+        {work, Master, Work} ->
+            Result = multiply(Work, Matrix),
+            Master ! {self(), Result}
+    after 200 ->
+              ok
+    end.
+gather_result([], Acc) ->
+    lists:reverse(Acc);
+gather_result([Pid | WorkerPids], Acc) ->
     receive
-        {Results} ->
-            Results
+        {Pid, Result} ->
+            gather_result(WorkerPids, [Result | Acc])
     end.
 
+    
 multiply_matrices([], _, ResultMatrix) ->
     lists:reverse(ResultMatrix);
 
@@ -62,43 +82,17 @@ flip_matrix([Head | Tail], Column, Rest, ListsOfColumns) when length(Head) > 1 -
 flip_matrix([Head | Tail], Column, Rest, ListsOfColumns) ->
      flip_matrix(Tail, [hd(Head)| Column], Rest, ListsOfColumns).
  
+split_matrix([], _, Acc) ->
+    lists:reverse(Acc);
 
-worker_pool(Master, [], Refs, _, _) ->
-    Results = [receive
-         {Ref, Result} -> Result 
-               after 20000 ->
-                       io:format("här gick det åt helvete")
-     end || Ref <- Refs],
-    Master ! {Results};
+split_matrix(Matrix, SplitValue, Acc) when length(Matrix) > (SplitValue*2-1) ->
+    {Chunk, Rest} = lists:split(SplitValue, Matrix),
+    split_matrix(Rest, SplitValue, [Chunk | Acc]);
 
-worker_pool(Master, WorkList, Refs, Matrix, SplitValue) when length(WorkList) > SplitValue->
-    {Work, Rest} = lists:split(SplitValue, WorkList),
-    receive
-        {ready, Pid} ->
-            Ref = make_ref(),
-            Pid ! {work, Ref, Work, Matrix},
-            worker_pool(Master, Rest, [Ref | Refs], Matrix, SplitValue)
-    end;
-
-worker_pool(Master, Work, Refs, Matrix, SplitValue) ->
-    receive
-        {ready, Pid} ->
-            Ref = make_ref(),
-            Pid ! {work, Ref, Work, Matrix},
-            worker_pool(Master, [], [Ref | Refs], Matrix, SplitValue)
-    end.
-worker(Master) ->
-    Master ! {ready, self()},
-    receive 
-        {work, Ref, Work, Matrix} ->
-            Result = multiply(Work, Matrix),
-            Master ! {Ref, Result},
-            worker(Master)
-    after 200 ->
-              ok
-    end.
+split_matrix(Matrix, SplitValue, Acc) ->
+    split_matrix([], SplitValue, [Matrix | Acc]).
 
 % Function to generate a matrix of given size
 generate_matrix(Size) ->
-    [[rand:uniform(100) || _ <- lists:seq(0, Size)] || _ <- lists:seq(0, Size)].
+    [[rand:uniform(100) || _ <- lists:seq(1, Size)] || _ <- lists:seq(1, Size)].
 
