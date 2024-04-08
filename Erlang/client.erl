7247ab9cc33cd77fe60cc586ba060a97e91fb8d4
RAHMSTAR-dev <fera6013@student.su.se>
2024-04-08 13:48:13 +0200

    Fixed Java the java server, fixed Erlang client

--- Erlang/client.erl
+++ Erlang/client.erl
@@ -10,37 +10,63 @@
 -author("Felix").
 
 %% API
--export([start/3, test_case_1/0]).
+-export([start/2, test_case_1/1]).
 
-start(Id, Timeout, Counter) ->
-  {ok, Socket} = gen_tcp:connect("localhost", 8000, [{active, false}, {mode, list}]),
+start(Id, Counter) ->
 
-  ok = gen_tcp:send(Socket, "Hello from Erlang!\n"),
+  case gen_tcp:connect("localhost", 8000, [{active, false}, {mode, list}]) of
+    {ok, Socket} ->
+      ok = gen_tcp:send(Socket, "Hello from Erlang!\n"),
 
-  case gen_tcp:recv(Socket, 0, Timeout * 1000) of
-    {ok, Data} ->
-      % io:fwrite("Client: ~p Received data: ~p~n", [Id, Data]);
-      ok;
-    {error, closed} ->
-      io:fwrite("Client : ~p Socket closed~n", [Id]);
-    {error, Reason} ->
-      % io:fwrite("Client: ~p Error: ~p~n", [Id, Reason]),
-      Counter ! {timeout}
-  end,
+      case gen_tcp:recv(Socket, 0) of
+        {ok, Data} ->
+          Counter ! {done};
+        {error, closed} ->
+          io:fwrite("Client : ~p Socket closed~n", [Id]);
+        {error, _} ->
+          Counter ! {timeout}
+      end,
 
-  ok = gen_tcp:close(Socket).
-
-test_case_1() ->
-  Pid = spawn(fun() -> counter(0) end),
-  [spawn(client, start, [Id, 10, Pid]) || Id <- lists:seq(1, 400)],
-  ok.
-  
-
-counter(N) -> 
-  receive 
-    {timeout} -> 
-      counter(N + 1)
-  after 7000 -> 
-          io:format("Amount of timeouts : ~p~n", [N])
+      ok = gen_tcp:close(Socket);
+    {error, _} ->
+      start(Id, Counter)
   end.
 
+
+
+test_case_1(N) ->
+  Self = self(),
+  Pid = spawn(fun() -> counter(N , Self) end),
+  Start = erlang:monotonic_time(nanosecond),
+  [spawn(client, start, [Id, Pid]) || Id <- lists:seq(1, N)],
+  receive
+    {done} ->
+      End = erlang:monotonic_time(nanosecond),
+      Duration = (End - Start) / 1_000_000_000,
+      receive
+        after 2000 ->
+          io:format("Computation time (sequential): ~f seconds~n", [Duration])
+      end
+  end,
+  ok.
+
+
+start_counter(0) ->
+  io:format("starting done");
+start_counter(N) ->
+  receive
+    {done} ->
+      start_counter(N - 1);
+    {started} ->
+      start_counter(N-1)
+
+  end.
+counter(0,Master) ->
+  Master ! {done};
+counter(N,Master) ->
+  receive
+    {done} ->
+      counter(N - 1, Master);
+    {started} ->
+      counter(N-1, Master)
+  end.
--- Java/Server.java
+++ Java/Server.java
@@ -1,25 +1,38 @@
 package Java;
 
-import java.io.BufferedOutputStream;
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server {
-    private static final boolean runParallel = true;
+    static final boolean runParallel = true;
+    static final boolean runParallelConnections = true;
+    static ServerSocket serverSocket;
+
+    private final static int amountOfThreads = 10;
     public static void main(String[] args) throws Exception {
-        try (ServerSocket serverSocket = new ServerSocket(8000)) {
-            while (true) {
-                try {
-                    Socket socket = serverSocket.accept();
-                    if (runParallel) {
-                        new Thread(new ClientHandler(socket)).start();
-                    } else {
-                        handleClientSeq(socket);
+        if (runParallelConnections) {
+            try {
+                serverSocket = new ServerSocket(8000);
+                for (int i = 0; i < amountOfThreads; i++) {
+                    new Thread(new ConnectionHandler(i+1)).start();
+                }
+            } catch (IOException e) {
+                System.out.println(e);
+            }
+        } else {
+            try (ServerSocket serverSocket = new ServerSocket(8000)) {
+                while (true) {
+                    try {
+                        Socket socket = serverSocket.accept();
+                        if (runParallel) {
+                            new Thread(new ClientHandler(socket)).start();
+                        } else {
+                            handleClientSeq(socket);
+                        }
+                    } catch (IOException e) {
+                        System.out.println("I/O error: " + e);
                     }
-                } catch (IOException e) {
-                    System.out.println("I/O error: " + e);
                 }
             }
         }
@@ -33,6 +46,12 @@ public class Server {
         try (PrintWriter out = new PrintWriter(new BufferedOutputStream(clientSocket.getOutputStream()))) {
             out.print("ok");
             out.flush();
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            String line = in.readLine();
+            while (line != null) {
+                line = in.readLine();
+            }
         } catch (IOException e) {
             System.out.println("Failed to send response to client: " + e.getMessage());
         } finally {
@@ -53,5 +72,32 @@ class ClientHandler implements Runnable {
     @Override
     public void run() {
         Server.handleRequest(clientSocket);
+    }
+}
+
+class ConnectionHandler implements Runnable {
+
+    private int workerNr;
+
+    public ConnectionHandler(int i) {
+        this.workerNr = i;
+    }
+    @Override
+    public void run() {
+        System.out.println("Started connection thread");
+        while (true) {
+            try {
+                Socket socket = Server.serverSocket.accept();
+                System.out.println("Thread " + workerNr + " accepted connection: " + socket.getInetAddress() + ":" + socket.getPort());
+                if (Server.runParallel) {
+                    new Thread(new ClientHandler(socket)).start();
+                } else {
+                    Server.handleClientSeq(socket);
+                }
+                System.out.println("Thread " + workerNr + " has handled its client");
+            } catch (IOException e) {
+                System.out.println("I/O error: " + e);
+            }
+        }
     }
 }
