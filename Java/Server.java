61b5f5fcb73cdcff119f7504d16ec3184e36e77a
RAHMSTAR-dev <fera6013@student.su.se>
2024-04-09 12:24:27 +0200

    Updated Java server

--- Java/Server.java
+++ Java/Server.java
@@ -6,30 +6,25 @@ import java.net.Socket;
 
 public class Server {
     static final boolean runParallel = true;
-    static final boolean runParallelConnections = true;
     static ServerSocket serverSocket;
 
-    private final static int amountOfThreads = 10;
+    private final static int amountOfThreads = 6;
     public static void main(String[] args) throws Exception {
-        if (runParallelConnections) {
+        if (runParallel) {
             try {
                 serverSocket = new ServerSocket(8000);
                 for (int i = 0; i < amountOfThreads; i++) {
-                    new Thread(new ConnectionHandler(i+1)).start();
+                    new Thread(new ConnectionHandler(i + 1)).start();
                 }
             } catch (IOException e) {
-                System.out.println(e);
+                System.out.println("I/O error: " + e);
             }
         } else {
             try (ServerSocket serverSocket = new ServerSocket(8000)) {
                 while (true) {
                     try {
                         Socket socket = serverSocket.accept();
-                        if (runParallel) {
-                            new Thread(new ClientHandler(socket)).start();
-                        } else {
-                            handleClientSeq(socket);
-                        }
+                        handleRequest(socket);
                     } catch (IOException e) {
                         System.out.println("I/O error: " + e);
                     }
@@ -38,18 +33,14 @@ public class Server {
         }
     }
 
-    static void handleClientSeq(Socket clientSocket) {
-        handleRequest(clientSocket);
-    }
-
     static void handleRequest(Socket clientSocket) {
         try (PrintWriter out = new PrintWriter(new BufferedOutputStream(clientSocket.getOutputStream()))) {
-            out.print("ok");
-            out.flush();
-
             BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
             String line = in.readLine();
             while (line != null) {
+                out.print("ok");
+                out.flush();
+                System.out.println(line);
                 line = in.readLine();
             }
         } catch (IOException e) {
@@ -62,42 +53,45 @@ public class Server {
             }
         }
     }
-}
 
-class ClientHandler implements Runnable {
-    private final Socket clientSocket;
-    public ClientHandler(Socket clientSocket) {
-        this.clientSocket = clientSocket;
+    static class ClientHandler implements Runnable {
+        private final Socket clientSocket;
+        public ClientHandler(Socket clientSocket) {
+            this.clientSocket = clientSocket;
+        }
+        @Override
+        public void run() {
+            Server.handleRequest(clientSocket);
+        }
     }
-    @Override
-    public void run() {
-        Server.handleRequest(clientSocket);
-    }
-}
 
-class ConnectionHandler implements Runnable {
+    static class ConnectionHandler implements Runnable {
 
-    private int workerNr;
+        private final int workerNr;
 
-    public ConnectionHandler(int i) {
-        this.workerNr = i;
-    }
-    @Override
-    public void run() {
-        System.out.println("Started connection thread");
-        while (true) {
-            try {
-                Socket socket = Server.serverSocket.accept();
-                System.out.println("Thread " + workerNr + " accepted connection: " + socket.getInetAddress() + ":" + socket.getPort());
-                if (Server.runParallel) {
-                    new Thread(new ClientHandler(socket)).start();
-                } else {
-                    Server.handleClientSeq(socket);
+        public ConnectionHandler(int i) {
+            this.workerNr = i;
+        }
+
+        @Override
+        public void run() {
+            System.out.println("Started connection thread");
+            while (true) {
+                try {
+                    Socket socket = serverSocket.accept();
+                    System.out.println("Thread " + workerNr + " accepted connection: " + socket.getInetAddress() + ":" + socket.getPort());
+
+                    for (int i = 0; i < amountOfThreads; i++) {
+                        new Thread(new ClientHandler(socket)).start();
+                    }
+
+                    System.out.println("Thread " + workerNr + " has handled its client");
+                } catch (IOException e) {
+                    System.out.println("I/O error: " + e);
                 }
-                System.out.println("Thread " + workerNr + " has handled its client");
-            } catch (IOException e) {
-                System.out.println("I/O error: " + e);
             }
         }
     }
 }
+
+
